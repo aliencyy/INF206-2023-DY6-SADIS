9ec56afc645dbdabd08c42bdb037a2c4be37f87b
Muhammad AKbarul Ihsan <barullisan@gmail.com>
2023-04-14 01:20:54 +0700

    feat: creat user and trash databases:

--- project-rpl/database/migrations/2023_04_13_181113_create_trashes_table.php
+++ project-rpl/database/migrations/2023_04_13_181113_create_trashes_table.php
@@ -1,4 +1,5 @@
  
+ 
 <?php
  
  use Illuminate\Database\Migrations\Migration;
@@ -12,13 +13,15 @@
       */
      public function up(): void
      {
-         Schema::create('users', function (Blueprint $table) {
+         Schema::create('subjects', function (Blueprint $table) {
              $table->id();
-             $table->string('name');
-             $table->string('email')->unique();
-             $table->string('password');
-             $table->string('lokasi');
-             $table->string('role')->default('user');
+             $table->foreignId('jenis_sampah');
+             $table->string('status_pengolahan');
+             $table->string('jenis_pengolahan');
+             $table->string('berat');
+             $table->string('ket');
+             $table->date('tanggal_pengambilan');
+             $table->timestamps();
          });
      }
   
@@ -27,7 +30,7 @@
       */
      public function down(): void
      {
-         Schema::dropIfExists('users');
+         Schema::dropIfExists('subjects');
      }
  };
   
