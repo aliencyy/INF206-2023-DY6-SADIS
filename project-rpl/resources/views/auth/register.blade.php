939bbc8bfb3585bad1745e2a4f9ae1638e169869
aliencyy <kelsyamirah62@gmail.com>
2023-04-14 09:57:33 +0700

    docs: modify register page

--- project-rpl/resources/views/auth/register.blade.php
+++ project-rpl/resources/views/auth/register.blade.php
@@ -6,8 +6,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Sadis | {{ $title }}</title>
-    <link rel="stylesheet" href="css/bootstrap.min.css" />
-    <link rel="stylesheet" href="style.css" />
+    <link rel="stylesheet" href="resources/css/app.css"/>
+    <link rel="stylesheet" href="resources/js/bootstrap.js">
+    @vite(['resources/sass/app.scss', 'resources/js/app.js','resources/js/bootstrap.js'])
 </head>
 
 <body>
