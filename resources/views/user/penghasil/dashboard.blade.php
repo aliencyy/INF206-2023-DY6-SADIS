3e17aa0d3258e64b44224e11b982e58fc7a3dc3d
Muhammad AKbarul Ihsan <barullisan@gmail.com>
2023-05-05 06:48:30 +0700

    feat: create table user for pengolah

--- resources/views/user/penghasil/dashboard.blade.php
+++ resources/views/user/penghasil/dashboard.blade.php
@@ -43,16 +43,16 @@
             <div class="panel-body p-5 rounded-6" style="background-color: #BFE4D3; border-radius: 10px;">
                 <div class="align-middle text-center"><h3>Pengolah Limbah B3 Aceh</h3>
 
-                       Area button under Pengolah Limbah B3 Aceh-->
                       <div class="container text-center">
                         <div class="row align-items-center p-4">
                             <!-- Button Orderan -->
                             <button  type="button" class="col p-3 mx-3 rounded-6 border-0" style="background-color: #A3D8BF; border-radius: 10px; ">
                                 <a href="/order" class="link-offset-2 link-offset-3-hover link-underline link-underline-opacity-0 link-underline-opacity-75-hover" style="color: black; font-size: 18px;">Orderan</a></button>
                             <!-- Button Daftar pelanggan-->
-                            <button type="button" class="col p-3 mx-3 rounded-6 border-0" style="background-color: #A3D8BF; border-radius: 10px;">
-                                <a href="#" class="link-offset-2 link-offset-3-hover link-underline link-underline-opacity-0 link-underline-opacity-75-hover" style="color: black; font-size: 18px;">Daftar Pelanggan</a></button>
-
+                            {{-- <button type="button" class="col p-3 mx-3 rounded-6 border-0" style="background-color: #A3D8BF; border-radius: 10px;">
+                                <a href="#" class="link-offset-2 link-offset-3-hover link-underline link-underline-opacity-0 link-underline-opacity-75-hover" style="color: black; font-size: 18px;">Daftar User</a></button> --}}
+                                <button type="button" class="col p-3 mx-3 rounded-6 border-0" style="background-color: #A3D8BF; border-radius: 10px;">
+                                    <a href="/listSubscriptor" class="link-offset-2 link-offset-3-hover link-underline link-underline-opacity-0 link-underline-opacity-75-hover" style="color: black; font-size: 18px;">Daftar Pelanggan</a></button>
                         </div>
                     </div>
                     <!-- END Area button Pengolah Limbah-->
