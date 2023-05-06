638f09f3f431f596061c23e2cba67e984a77d866
Annisa Lathifa <annisalathifa1234@gmail.com>
2023-05-06 19:42:14 +0700

    feat: modify profil file

--- resources/views/user/profil.blade.php
+++ resources/views/user/profil.blade.php
@@ -1,37 +1,99 @@
-<!DOCTYPE html>
-<html lang="en">
 
-<head>
-    <meta charset="UTF-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>SADIS</title>
-    <link rel="stylesheet" href="resources/css/app.css"/>
-    <link rel="stylesheet" href="resources/js/bootstrap.js">
-    @vite(['resources/sass/app.scss', 'resources/js/app.js','resources/js/bootstrap.js'])
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"/>
-</head>
+@extends('layouts.app')
+@section('content')
+<body style="background-color: #D9F9EE;">
 
-<style>
-    * {
-    margin: 0;
-    padding: 0;
-    box-sizing: border-box;
-}
+<div class="modal position-static d-block py-5">
+  <div class="modal-dialog" >
+    <div class="modal-content rounded-4 shadow border-0 mb-2" style="background-color: #FDFEF3">
+      <div class="modal-header p-5 pb-4 border-bottom-0">
 
-body {
-    background: #D9F9EE;
-    padding: 20px 20px 15px 15px;
-}
+        <h1 class="fw-bolder mb-0 fs-1">Profile</h1>
+      </div>
 
-</style>
+      <!-- START Isi Form-->
+      <div class="modal-body p-5 pt-0">
 
-<body>
-    <div class="PageProfilUser">
-        <p style="text-align:justify;"><img src="logo_rumah_sakit.png" style="float:left; margin:0 9px 3px 0;" width="60" height="80px"/>RS Sehat<br/> Banda Aceh, Aceh</p>
+          <div class="mb-3">
+
+            <!-- Start Nama Instansi User Name Penghasil-->
+            <div class="text-center">
+              <img src="img/icon-penghasil.png" class="object-fit-md-contain border rounded" alt="Rumah Sakit Sehat" width="200">
+              <h2 class="fs-3 fw-border mt-4">Rumah Sakit Sehat</h2>
+            </div>
+            <!-- End Nama Instansi User Name Penghasil-->
+
+            <!-- Start Nama Instansi User Name Pengolah-->
+
+            {{-- <!-- Nama ADMIN User Name Pengolah-->
+            <div class="text-center">
+              <img src="img/icon-pengolah.jpg" class="object-fit-md-contain border rounded" alt="Rumah Sakit Sehat" width="200">
+              <h2 class="fs-3 fw-border mt-4">Admin</h2>
+            </div> --}}
+
+            <!-- End Nama Instansi User Name Pengolah-->
+
+            <hr class="my-1">
+            
+
+              <div class="container overflow-hidden mt-4 text-start">
+                
+                <!-- Start Email User penghasil -->
+                <div class="row">
+                  <div class="col-3">
+                  <div class="p-2" style="font-size: 16px">Email</div>
+                  </div>
+                  <div class="col">
+                    <div class="p-2" style="font-size: 16px">rumahsakitsehat@gmail.com</div>
+                  </div>
+              </div>
+              <!-- End Email User penghasil -->
+
+                <!-- Start Email User pengolah -->
+                {{-- <div class="row">
+                    <div class="col-3">
+                        <div class="p-2" style="font-size: 16px">Email</div>
+                    </div>
+                    <div class="col">
+                        <div class="p-2" style="font-size: 16px">adminsadis@gmail.com</div>
+                    </div>
+                </div> --}}
+                <!-- End Email User pengolah -->
+
+                  <!-- Start Lokasi User Penghasil -->
+                  <div class="row">
+                    <div class="col-3">
+                    <div class="p-2" style="font-size: 16px">Lokasi</div>
+                    </div>
+                    <div class="col">
+                      <div class="p-2" style="font-size: 16px">Banda Aceh, Jln. Topi No. 2</div>
+                    </div>
+                </div>
+                <!-- End Lokasi User Penghasil -->
+                
+                <!-- Start Lokasi User Penghasil -->
+                {{-- <div class="row">
+                    <div class="col-3">
+                        <div class="p-2" style="font-size: 16px">Lokasi</div>
+                    </div>
+                    <div class="col">
+                        <div class="p-2" style="font-size: 16px">Banda Aceh, Jln. Topi No. 2</div>
+                    </div>
+                </div> --}}
+                <!-- End Lokasi User Penghasil -->
+
+
+              </div>
+              <!-- END ISIAN -->
+            </div>
+
+
+      </div>
     </div>
+    </div>
+  </div>
+</div>
 
-    <script src="js/bootstrap.bundle.min.js"></script>
-</body>
 
-</html>
+
+@endsection
