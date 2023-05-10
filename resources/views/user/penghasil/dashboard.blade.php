da7d72566a98fc5fc364637d8f696d9a323b9ad1
Kelsy Amirah <kelsy@mhs.unsyiah.ac.id>
2023-05-10 13:37:41 +0800

    docs: modify dashboard (penghasil)

--- resources/views/user/penghasil/dashboard.blade.php
+++ resources/views/user/penghasil/dashboard.blade.php
@@ -9,7 +9,7 @@
 
 @can('user')
       <!-- Isi dashboard penghasil -->
-  <div class="container p-5" style="margin-top: 200px;">
+  <div class="container p-5 mt-3">
     <div class="panel panel-default position-relative" style="border-radius: 30px;">
         <div class="panel-body p-5 rounded-6" style="background-color: #BFE4D3; border-radius: 10px;">
             <div class="align-middle text-center"><h3>Pengolah Limbah B3 Aceh</h3>
@@ -19,13 +19,13 @@
                     <div class="row align-items-center p-4">
                         <!-- Button Orderan -->
                         <button  type="button" class="col p-3 mx-3 rounded-6 border-0" style="background-color: #A3D8BF; border-radius: 10px; ">
-                            <a href="/buangsampah" class="link-offset-2 link-offset-3-hover link-underline link-underline-opacity-0 link-underline-opacity-75-hover" style="color: black; font-size: 18px;">Buat Pengajuan pengambilan sampah</a></button>
+                            <a href="/buangsampah" class="text-decoration-none" style="color: black; font-size: 18px;">Buat Pengajuan pengambilan sampah</a></button>
                         <!-- Button Daftar pelanggan-->
                         <button type="button" class="col p-3 mx-3 rounded-6 border-0" style="background-color: #A3D8BF; border-radius: 10px;">
-                            <a href="/order" class="link-offset-2 link-offset-3-hover link-underline link-underline-opacity-0 link-underline-opacity-75-hover" style="color: black; font-size: 18px;">Riwayat pembuangan sampah</a></button>
+                            <a href="/order" class="text-decoration-none" style="color: black; font-size: 18px;">Riwayat pembuangan sampah</a></button>
                         
                         <button type="button" class="col p-3 mx-3 rounded-6 border-0" style="background-color: #A3D8BF; border-radius: 10px;">
-                            <a href="/langganan" class="link-offset-2 link-offset-3-hover link-underline link-underline-opacity-0 link-underline-opacity-75-hover" style="color: black; font-size: 18px;">Subscription</a></button>
+                            <a href="/langganan" class="text-decoration-none" style="color: black; font-size: 18px;">Subscription</a></button>
                     </div>
                 </div>
                 <!-- END Area button Pengolah Limbah-->
